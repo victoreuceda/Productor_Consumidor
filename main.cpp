8acffdd70b04aad4a0f4391a5aede19a473c11f1
herbertpaz13 <herbertpaz13@outlook.com>
2017-10-14 10:26:26 -0600

    hilos

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdlib>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>    
+#include <time.h> 
+
+void *RunC(void *threadid);
+void *RunP(void *threadid);
+
+int main(){
+    pthread_t Productor;
+    pthread_t Consumidor;
+    int i=0;
+    pthread_create(&Productor,NULL, RunP, (void *)i);
+    i=1;
+    pthread_create(&Consumidor,NULL, RunC, (void *)i);
+    for(;;){
+        bufer.imprimir();
+        sleep(5);
+    }
+    pthread_exit(NULL);
+    return 0;
+}
+
+void *RunP(void *threadid){
+    int x;
+    for(;;){
+        srand (time(NULL));
+        x= rand() % 100;
+        bufer.agregar(x);
+        sleep(2);
+    }
+}
+
+void *RunC(void *threadid){
+    for(;;){
+        bufer.sacar();
+        sleep(3);
+    }
+}
